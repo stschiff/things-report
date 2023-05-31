b96f3c9f2dcc63fd67a84e3cb7460dbcc031f644
Stephan Schiffels <stephan_schiffels@eva.mpg.de>
2023-05-31 09:36:26 +0200

    continued

--- things-report.py
+++ things-report.py
@@ -2,6 +2,13 @@
 
 import things
 
+print("#Deadlines")
+
+
+for task in things.deadlines():
+    print('-', task['type'], task['title'], task['start_date'], task['deadline'])
+
+
 for task in things.upcoming():
     print(task['type'], task['title'], task['start_date'], task['deadline'])
-    print(task)
+    # print(task)
