ffdeb7775db27b0632be01f3cfc289cfa65d8e7f
Stephan Schiffels <stephan_schiffels@eva.mpg.de>
2023-06-02 10:53:58 +0200

    m

--- things-report.py
+++ things-report.py
@@ -2,50 +2,29 @@
 
 import things
 
-print("#Deadlines\n")
+print("# Deadlines\n")
 
 for task in things.deadlines():
-    print('-', task['type'], task['title'], task['start_date'], task['deadline'])
+    print('- [ ]', task['type'], task['title'], task['start_date'], task['deadline'])
+print("")
 
-print("\n#Upcoming\n")
+print("# Upcoming\n")
 
 for task in things.upcoming():
-    print('-', task['type'], task['title'], task['start_date'])
-    # print(task)
+    print('- [ ]', task['type'], task['title'], task['start_date'])
+print("")
 
-print("\n#Digital\n")
+print("# Digital\n")
 
-for task in things.tasks(tag="Digital"):
-    print('-', task['type'], task['title'])
+for t in ["Digital", "coding", "schreiben", "Recherche", "Email/Chat"]:
+    print("##", t, "\n")
+    for task in things.tasks(tag=t):
+        print('- [ ]', task['title'])
+    print("")
 
-print("\n#Planen / Denken\n")
-for task in things.tasks(tag="planen/denken"):
-    print('-', task['type'], task['title'])
-
-print("\n#lesen\n")
-for task in things.tasks(tag="lesen"):
-    print('-', task['type'], task['title'])
-
-print("\n#Anrufe\n")
-for task in things.tasks(tag="Anrufe"):
-    print('-', task['type'], task['title'])
-
-print("\n#Unterwegs\n")
-for task in things.tasks(tag="unterwegs 🏃🏽"):
-    print('-', task['type'], task['title'])
-
-print("\n#Daheim\n")
-for task in things.tasks(tag="daheim 🏡"):
-    print('-', task['type'], task['title'])
-
-print("\n#Institut\n")
-for task in things.tasks(tag="Institut 🏢"):
-    print('-', task['type'], task['title'])
-
-print("\n#Warten auf\n")
-for task in things.tasks(tag="Warten auf ⌛️"):
-    print('-', task['type'], task['title'])
-
-print("\n#Agendas\n")
-for task in things.tasks(tag="Agendas🕰"):
-    print('-', task['type'], task['title'])
+for t in ["planen/denken", "lesen", "Anrufe", "unterwegs 🏃🏽", "daheim 🏡",
+          "Institut 🏢", "Warten auf ⌛️"]:
+    print("#", t, "\n")
+    for task in things.tasks(tag=t):
+        print('- [ ]', task['title'])
+    print("")
